c661fe067c3296b0e786fdd87ab75effaa3500da
Fabian-Cravero <168470105+Fabian-Cravero@users.noreply.github.com>
2024-06-30 23:32:59 -0300

    v.1.0

--- Scrips.js
+++ Scrips.js
@@ -1,20 +1,34 @@
+function update(){
+    location.reload();
+    document.getElementById('mostrar-datos').style.display = 'block';
+}
+
+window.addEventListener('load', mostrarAdministrador);
+document.getElementById('mostrar-datos').style.display = 'block';
 //api
 
 let url = 'https://api.yumserver.com/17028/generic/stoks';
 
-window.addEventListener('load', mostrarAdministrador);
 
-var ids =['mostrar-datos', 'Cargar-Nuevo', 'Modificar-Producto'];
-function Mostrar(_div){
-    for (let i = 0; i < ids.length; i++){
-        document.getElementById(ids[i].setAttribute('style', 'display:none'))
-    }
-    document.getElementById(_div).removeAttribute('style')
-    
-}
 
 //Mostrar datos
+function abrir_cerrar_Datos(){
+    const mostrar = document.getElementById('mostrar-datos');
+    if (mostrar){
+       if (mostrar.style.display == 'block')
+       {
+          mostrar.style.display = 'none';
+       }
+       else{
+          mostrar.style.display = 'block';
+          document.getElementById('Cargar-Nuevo').style.display = 'none';
+          document.getElementById('Modificar-Producto').style.display = 'none';
+       }
+    }
+    }
+
 function mostrarAdministrador(){
+    
     fetch(url)
     .then(response => response.json())
     .then(Mostrardatos)
@@ -32,6 +46,7 @@ function Mostrardatos(data){
                 <td class="table-administrador">${data[i].param3}</td>
                 <td class="table-administrador">${data[i].param4}</td>
                 <td><Button onclick="Borrar('${data[i].idcod}')">Borrar</button></td>
+                <td><Button onclick="Abrir_Modificar('${data[i].idcod}')">Modificar</button></td>
             </tr>
             `;            
         }
@@ -40,7 +55,20 @@ function Mostrardatos(data){
 
 
 // Crear un nuevo producto
-
+function abrir_cerrar_Nuevo(){
+    const mostrar = document.getElementById('Cargar-Nuevo');
+    if (mostrar){
+       if (mostrar.style.display == 'block')
+       {
+          mostrar.style.display = 'none';
+       }
+       else{
+          mostrar.style.display = 'block';
+          document.getElementById('mostrar-datos').style.display = 'none';
+          document.getElementById('Modificar-Producto').style.display = 'none';
+       }
+    }
+    }
 function GuardarProducto(){
     
     let nuevo = {
@@ -59,8 +87,9 @@ function GuardarProducto(){
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error))
     alert('Guardado Correctament');
-    location.reload();
+    update();
 }
+
 
 //Borrar
 function Borrar(obj) {
@@ -75,18 +104,68 @@ function Borrar(obj) {
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error))
         alert('Eliminado correctamente');
+        update()
     }else{
         alert('eliminacion cancelada');
     }
-    location.reload();
+    
     }
 
 
 //Modificar
-function Abrir_Modificar(){
+function Abrir_Modificar(obj){
+    
+    if (obj !== ""){
+        fetch('https://api.yumserver.com/17028/generic/stoks')
+        .then(response => response.json())
+        .then(data => {
+        for (let i = 0; i < data.length; i++){
+            if(data[i].idcod === obj){
+                document.getElementById("IdCod_Modificar").value = data[i].idcod;
+                document.getElementById("Titulo_Modificar").value = data[i].param1;
+                document.getElementById("Precio_Modificar").value = data[i].param2;
+                document.getElementById("Stock_Modificar").value = data[i].param3;
+                document.getElementById("Fecha_Modificar").value = data[i].param4;
 
+                document.getElementById('Cargar-Nuevo').style.display = 'none';
+                document.getElementById('mostrar-datos').style.display = 'none';
+                document.getElementById('Modificar-Producto').style.display = 'block';
+                return;
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert("El Idcod no existe");
+    });
+    }else {alert("Ingrese un idcod");}
+    
 }
 
+function Guardar_Modificacion(){
+    var Modificar = {
+        idcod: document.getElementById("IdCod_Modificar").value,
+        param1: document.getElementById("Titulo_Modificar").value,
+        param2: document.getElementById("Precio_Modificar").value,
+        param3: document.getElementById("Stock_Modificar").value,
+        param4: document.getElementById("Fecha_Modificar").value.replace(/-/g, '.')
+    }
+    fetch(url,{
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Modificar)
+    })
+    .then(response =>response.text())
+    .then(data => {
+        console.log(data);
+        if(data === "OK"){
+            alert('Se guardo Correctamente')
+            update();
+        }else{alert('No se guardo')}
+    })
+    .catch(error => console.error('error', error));
+    document.getElementById("IdCod_Modificar").value = "";
+}
     
 
 
